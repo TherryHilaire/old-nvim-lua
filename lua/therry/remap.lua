2b4a350ecc7e80b01b5e11a92ac9b95216b379c1
TherryHilaire <therryleseul@gmail.com>
2022-11-07 14:57:04 -0500

    I forgot

--- lua/therry/remap.lua
+++ lua/therry/remap.lua
@@ -26,14 +26,14 @@ nnoremap("<leader>pu", "<cmd>PackerUpdate<cr>")
 nnoremap("<esc>", "<cmd>nohlsearch<cr>")
 
 -- Telescope
-nnoremap("<C-p>", ":Telescope")
+-- nnoremap("<C-p>", ":Telescope")
 nnoremap("<leader>ps", function()
     require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})
 end)
-nnoremap("<C-p>", function()
+nnoremap("<Leader>pf", function()
     require('telescope.builtin').git_files()
 end)
-nnoremap("<Leader>pf", function()
+nnoremap("<C-p>", function()
     require('telescope.builtin').find_files()
 end)
 
